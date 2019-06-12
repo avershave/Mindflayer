6764ff9a1e17c9be0f04dc28751fa87a173d1e3e
avershave <austin.vershave@gmail.com>
2019-06-12 11:18:44 -0400

    ...

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+__author__ = "Austin Vershave"
+__datestarted__ = "June 12th 2019"
+__email__ = "austin.vershave@gmail.com"
---
+++ pyRon.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import msgpack
+import httplib
+import metasploit

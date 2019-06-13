d399644870404d61a8d2f240d39b0384f4302e77
avershave <austin.vershave@gmail.com>
2019-06-12 20:48:13 -0400

    ...

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 
 __author__ = "Austin Vershave"
-__datestarted__ = "June 12th 2019"
 __email__ = "austin.vershave@gmail.com"
--- pyRon.py
+++ pyRon.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python
-
-import msgpack
-import httplib
-import metasploit
+from clientConnect import ConnectClient
+from metasploit.msfrpc import MsfRpcClient

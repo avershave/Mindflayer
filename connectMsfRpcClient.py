e827f17bbdb036475a0192ede91d01138092cb97
avershave <austin.vershave@gmail.com>
2019-06-21 08:56:16 -0400

    python3

--- connectMsfRpcClient.py
+++ connectMsfRpcClient.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #Credit goes to Luis Hebendanz
 #https://github.com/Luis-Hebendanz/msf-remote-console
 
-from metasploit.msfrpc import MsfRpcClient
-from metasploit.msfrpc import MsfRpcError
+from pymetasploit3.msfrpc import MsfRpcClient
+from pymetasploit3.msfrpc import MsfRpcError
 from ssl import SSLError
 import socket
 
@@ -19,23 +19,23 @@ class connectMsfRpcClient:
 
     # Connect to the msfrpcd server
     def connect(self):
-        print "[*] Connecting to server:\n Host => %s,\n Port => %s,\n User => %s,\n " \
-              "Pwd => %s,\n SSL => %s\n" % (self.host, self.port, self.username, '*' * len(self.password), self.ssl)
+        print ("[*] Connecting to server:\n Host => %s,\n Port => %s,\n User => %s,\n " \
+              "Pwd => %s,\n SSL => %s\n" % (self.host, self.port, self.username, '*' * len(self.password), self.ssl))
         # Login to msfrpcd server
         try:
             kwargs = {'username': self.username, 'port': self.port, 'server': self.host, 'ssl': self.ssl}
             self.client = MsfRpcClient(self.password, **kwargs)
-            print "[+] Successfully connected"
-        except SSLError, msg:
-            print "[-] SSL error: " + str(msg)
-            print "[-] You probably have installed the wrong pymetasploit version try installing it from here: https://github.com/allfro/pymetasploit.git"
+            print ("[+] Successfully connected")
+        except SSLError as msg:
+            print ("[-] SSL error: " + str(msg))
+            print ("[-] You probably have installed the wrong pymetasploit version try installing it from here: https://github.com/allfro/pymetasploit.git")
             return False
-        except socket.error, msg:
-            print "[-] Couldn't connect to server: " + str(msg)
+        except socket.error as msg:
+            print ("[-] Couldn't connect to server: " + str(msg))
             return False
         except MsfRpcError:
-            print "[-] Login failed. Wrong username or password"
+            print ("[-] Login failed. Wrong username or password")
             return False
         self.console = self.client.consoles.console()
         self.console_id = self.console.cid
-        print "[*] Console ID: " + self.console_id
+        print ("[*] Console ID: " + self.console_id)
--- pyRon.py
+++ pyRon.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #Credit goes to Luis Hebendanz
 #https://github.com/Luis-Hebendanz/msf-remote-console
-from __future__ import print_function
+
 try:
     import time
     import sys
-    import socket
-    from ssl import SSLError
-    from threading import Timer, Lock
 except ImportError as msg:
     print ("[-] Library not installed: " + str(msg))
-    print ("[*] Try installing it with: pip install " + str(msg.message))
+    print ("[*] Try installing it with: pip install " + str(msg.msg))
     sys.exit()
 
 try:
-    try:
-      import readline
-    except ImportError:
-      import pyreadline as readline
-except ImportError:
-    print ("[-] Readline module is not installed!")
-    print ("[*] Install on Linux with: pip install readline")
-    print ("[*] Install on Windows with: pip install pyreadline")
-    sys.exit()
-
-try:
-    from metasploit.msfrpc import MsfRpcError, MsfRpcClient
-    from metasploit.msfconsole import MsfRpcConsole
+    from pymetasploit3.msfrpc import MsfRpcError, MsfRpcClient
+    from pymetasploit3.msfconsole import MsfRpcConsole
     from connectMsfRpcClient import connectMsfRpcClient
 except ImportError as msg:
     print ("[-] Missing library pymetasploit")
@@ -45,7 +31,7 @@ class pyRon:
     def __init__(self):
         # Adding Customization OR using defaults       
         try:
-            useDefaults = raw_input("[DEFAULTS] Would you like to use the all defaults? y/n: ").upper()
+            useDefaults = input("[DEFAULTS] Would you like to use the all defaults? y/n: ").upper()
             if useDefaults == 'Y':
                 self.username = 'msf'
                 self.password = 'msf'
@@ -54,29 +40,29 @@ class pyRon:
                 self.ssl = True
             elif useDefaults == 'N':
                 print ('Press enter for individual defaults')
-                self.username = raw_input('[Set Username] Please enter the username: ')
+                self.username = input('[Set Username] Please enter the username: ')
                 if self.username == '':
                     print ('[Set Username] Using default username: msf')
                     self.username = 'msf'
-                self.password = raw_input('[Set Password] Please enter the password: ')
+                self.password = input('[Set Password] Please enter the password: ')
                 if self.password == '':
-                    self.emptyPasswordChoice == raw_input('[Set Password] Would you like to set an empty password? y/n: ').upper()
+                    self.emptyPasswordChoice == input('[Set Password] Would you like to set an empty password? y/n: ').upper()
                     if self.emptyPasswordChoice == 'Y':
                         self.password = ''
                     elif self.emptyPasswordChoice == 'N':
                         print ('[Set Password] Using default password: msf')
                         self.password = 'msf'
-                self.port = raw_input('[Set Port] Please enter the port: ')
+                self.port = input('[Set Port] Please enter the port: ')
                 if self.port == '':
                     print('[Set Port] Using default port: 55553')
                     self.port = 55553
                 else:
                     self.port = int(self.port)
-                self.host = raw_input('[Set Host] Please select the host: ')
+                self.host = input('[Set Host] Please select the host: ')
                 if self.host == '':
                     print ('[Set Host] Using default host: 127.0.0.1')
                     self.host = '127.0.0.1'
-                setSSL = raw_input('[Set SSL] Using ssl? t/F: ').upper()
+                setSSL = input('[Set SSL] Using ssl? t/F: ').upper()
                 if setSSL == '':
                     print ('[Set SSL] Using default: True')
                     self.ssl = True
@@ -103,23 +89,23 @@ class pyRon:
         # Testing sending commands to console to run
         # THIS WORKS FOR EXPLOITS but needs EXE to run
         print ("USING EXPLOIT")
-        exploit = raw_input("Please enter exploit: ")
+        exploit = input("Please enter exploit: ")
         self.msfclient.console.write('use ' + exploit)
         time.sleep(2)
         print ("SET PAYLOAD")
-        payload = raw_input("Please enter payload: ")
+        payload = input("Please enter payload: ")
         self.msfclient.console.write('set payload ' + payload)
         time.sleep(2)
         print("SET LHOST")
-        host = str(raw_input("Please enter LHOST: "))
+        host = str(input("Please enter LHOST: "))
         self.msfclient.console.write('set LHOST ' + host)
         time.sleep(2)
         print("SET LPORT")
-        port = str(raw_input("Please enter LPORT: "))
+        port = str(input("Please enter LPORT: "))
         self.msfclient.console.write('set LPORT ' + port)
         time.sleep(2)
         print("SET THREAD")
-        thread = str(raw_input("Please enter number of threads: "))
+        thread = str(input("Please enter number of threads: "))
         self.msfclient.console.write('set THREADS ' + thread)
         time.sleep(2)
         print("RUNNING")
@@ -131,12 +117,12 @@ class pyRon:
         # Params: Pass=password, default port is 55552
         self.sessions = self.msfclient.client.sessions.list
         for s_id, s_info in self.sessions.items():
-            print("\nSession ID: ", s_id)
-            
+            print("\nSession ID: ", s_id)  
             for key in s_info:
                 print(key + ':', s_info[key])
 
     def mainMenu(self):
+        '''MainMenu'''
         menuGoing = False
         print("[+] Console Running and Connected!\n")
         print("[!] Entering Main Menu\n")
@@ -145,7 +131,7 @@ class pyRon:
             print("1.) Start Exploit and Payload")
             print("2.) Print Current Session")
             print("press 0 to exit...")
-            selection = input("[!] Please select an option: ")
+            selection = int(input("[!] Please select an option: "))
             if selection == 1:
                 self.execteSimpleExploit()
             if selection == 2:
